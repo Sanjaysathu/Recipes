5658e32471914db9b7c145e930c40804a8e2a61a
sanjaysathu <snjsathu@gmail.com>
2019-12-02 18:45:05 +0530

    Updates

---
+++ precache-manifest.eb7bef0b32b92ba6414db5b3e43d696e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff05894afa6e336621c30a7bd8dc43ee",
+    "url": "/Recipes/index.html"
+  },
+  {
+    "revision": "6e6374e0c0c98ef31881",
+    "url": "/Recipes/static/css/main.98817c5d.chunk.css"
+  },
+  {
+    "revision": "018aa86b4f6a324a36a9",
+    "url": "/Recipes/static/js/2.eef861fe.chunk.js"
+  },
+  {
+    "revision": "6e6374e0c0c98ef31881",
+    "url": "/Recipes/static/js/main.7f0fe50e.chunk.js"
+  },
+  {
+    "revision": "925fc14a9a836ae742cb",
+    "url": "/Recipes/static/js/runtime-main.ca3af7bd.js"
+  }
+]);
